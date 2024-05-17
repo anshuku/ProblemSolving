83eea8c81887271a1c5e8214c38aa915597e1aa4
anshukumar <anshukumar@localhost>
2024-05-17 22:15:18 +0530

    Increasing triplet subsequence, count good triplets, arithmetic triplets

--- ProblemSolving/src/main/java/LeetCode/Arrays/P334IncreasingTripletSubsequence.java
+++ ProblemSolving/src/main/java/LeetCode/Arrays/P334IncreasingTripletSubsequence.java
@@ -1,29 +1,32 @@
 package LeetCode.Arrays;
 
+/*
+ * Given an integer array nums, return true if there exists a triple of indices (i, j, k) 
+ * such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+ */
 public class P334IncreasingTripletSubsequence {
 
 	public static void main(String[] args) {
 
-//		int[] nums = { 1, 2, 3, 4, 5 };
+		int[] nums = { 1, 2, 3, 4, 5 };
 //		int[] nums = { 5, 4, 3, 2, 1 };
 //		int[] nums = { 2, 1, 5, 0, 4, 6 };
 //		int[] nums = { 2, 4, -2, -3 };
 //		int[] nums = { 1, 5, 0, 4, 1, 3 };
 //		int[] nums = { 1, 1, -2, 6 };
-		int[] nums = { 20, 100, 10, 12, 5, 13 };
+//		int[] nums = { 20, 100, 10, 12, 5, 13 };
 
 		P334IncreasingTripletSubsequence pts = new P334IncreasingTripletSubsequence();
 		boolean subsequenceExists = pts.increasingTriplet(nums);
 		System.out.println("The increasing triplet subsequence exists " + subsequenceExists);
-
 	}
 
 	private boolean increasingTriplet(int[] nums) {
 		int a = Integer.MAX_VALUE, b = Integer.MAX_VALUE;
 		for (int num : nums) {
-			if (num <= a) {// = other wise usecase where there repeating number use case returns wrong result
+			if (num <= a) {// = other wise use case where there is repeating number returns wrong result
 				a = num;
-			} else if (num <= b) {// = other wise it fails for usecase: 1, 1, -2, 6
+			} else if (num <= b) {// = other wise it fails for use case: 1, 1, -2, 6
 				b = num;
 			} else {
 				return true;
