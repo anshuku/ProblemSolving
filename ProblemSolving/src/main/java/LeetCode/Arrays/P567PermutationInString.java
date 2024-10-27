bdc10df6e71514d0f0818af9ad43ead8ef4c47a9
anshukumar <anshukumar@localhost>
2024-10-27 08:55:40 +0530

    Minor changes

--- ProblemSolving/src/main/java/LeetCode/Arrays/P567PermutationInString.java
+++ ProblemSolving/src/main/java/LeetCode/Arrays/P567PermutationInString.java
@@ -8,7 +8,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-/* P.567 Permutation in String - Medium
+/* 
+ * P.567 Permutation in String - Medium
  * Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
  * In other words, return true if one of s1's permutations is the substring of s2.
  */
---
+++ ProblemSolving/src/main/java/LeetCode/LinkedLists/P206ReverseLinkedList.java
@@ -0,0 +1,98 @@
+package LeetCode.LinkedLists;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * Given the head of a singly linked list, reverse the list, and return the reversed list.
+ * 
+ * LOD - Easy
+ */
+public class P206ReverseLinkedList {
+
+	static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		head.next.next.next = new ListNode(4);
+		head.next.next.next.next = new ListNode(5);
+
+//		ListNode result = reverseLinkedList(head);
+
+		ListNode result = reverseLinkedListRecursive(head);
+
+//		ListNode result = reverseLinkedListIterative(head);
+
+		while (result != null) {
+			System.out.print(result.val + "->");
+			result = result.next;
+		}
+	}
+
+	private static ListNode reverseLinkedListIterative(ListNode head) {
+
+		ListNode curr = head; // 1 2 3 4 5
+		ListNode next = null;
+		ListNode prev = null;
+		while (curr != null) {// n cn p c
+			next = curr.next; // 2 3 4 5 -> 3 4 5 	-> 4 5		-> 5		-> 
+			curr.next = prev; // 1		 -> 2 1		-> 3 2 1	-> 4 3 2 1	-> 5 4 3 2 1
+			prev = curr; // 1			 -> 2 1		-> 3 2 1	-> 4 3 2 1	-> 5 4 3 2 1
+			curr = next; // 2 3 4 5		 -> 3 4 5	-> 4 5		-> 5		-> 
+
+		}
+		return prev;
+	}
+
+	private static ListNode reverseLinkedListRecursive(ListNode head) {
+		if (head == null || head.next == null) {
+			return head;
+		}
+		ListNode temp = reverseLinkedListRecursive(head.next);
+
+		head.next.next = head;
+
+		head.next = null;
+
+		return temp;
+	}
+
+	private static ListNode reverseLinkedList(ListNode head) {
+		ListNode node = head;
+		int counter = 0;
+		List<ListNode> nodes = new ArrayList<>();
+		while (node.next != null) {
+			counter++;
+			nodes.add(new ListNode(node.val));
+			node = node.next;
+		}
+		System.out.println("The value of node is " + node.val + " counter is " + counter);
+		System.out.println("size of nodes is " + nodes.size());
+		ListNode result = node;
+		for (int i = counter - 1; i >= 0; i--) {
+			node.next = nodes.get(i);
+			node = node.next;
+		}
+		return result;
+	}
+
+}
