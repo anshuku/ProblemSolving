13fb700f463dd95223d56c885af209c29f4ec7a8
anshukumar <anshukumar@localhost>
2024-12-01 16:57:47 +0530

    Minor changes

--- ProblemSolving/src/main/java/LeetCode/Stack/P735AsteroidCollision.java
+++ ProblemSolving/src/main/java/LeetCode/Stack/P735AsteroidCollision.java
@@ -12,6 +12,8 @@ import java.util.Stack;
  * Find out the state of the asteroids after all collisions. If two asteroids meet, 
  * the smaller one will explode. If both are the same size, both will explode. 
  * Two asteroids moving in the same direction will never meet.
+ * 
+ * Approach - Stack
  */
 public class P735AsteroidCollision {
 
