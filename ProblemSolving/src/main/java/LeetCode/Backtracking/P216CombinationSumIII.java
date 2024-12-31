112723c345cf7395f150a1e3d9893a7e218b4390
anshukumar <anshukumar@localhost>
2024-12-31 06:53:29 +0530

    Minor changes

--- ProblemSolving/src/main/java/LeetCode/Backtracking/P216CombinationSumIII.java
+++ ProblemSolving/src/main/java/LeetCode/Backtracking/P216CombinationSumIII.java
@@ -21,7 +21,8 @@ public class P216CombinationSumIII {
 	public static void main(String[] args) {
 
 //		int k = 3, n = 7;
-		int k = 3, n = 9;
+//		int k = 3, n = 9;
+		int k = 3, n = 15;
 
 		List<List<Integer>> list = combinationSum3(k, n);
 
@@ -41,12 +42,12 @@ public class P216CombinationSumIII {
 			result.add(new ArrayList<Integer>(list));
 			return;
 		}
-		for (int i = start; i <= 9; i++) {
+		for (int i = start; i <= 9; i++) { // i = start means the elements will be unique
 			if (list.size() >= k || i > n) {
 				break;
 			}
 			list.add(i);
-			recursive(result, list, i + 1, k, n - i);
+			recursive(result, list, i + 1, k, n - i); // start = i+1 means check all next elements from start
 			list.remove(list.size() - 1);
 		}
 	}
