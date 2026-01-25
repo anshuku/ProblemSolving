0095ee4143b72d30a50365851c4d8732712de5ec
anshukumar <anshukumar@localhost>
2026-01-25 21:16:44 +0530

    Keys and Rooms - DFS - Stack/Recursion

--- ProblemSolving/src/main/java/LeetCode/Graphs/P841KeysAndRooms.java
+++ ProblemSolving/src/main/java/LeetCode/Graphs/P841KeysAndRooms.java
@@ -34,8 +34,17 @@ public class P841KeysAndRooms {
 
 		boolean canVisitStack = canVisitAllRoomsStack(rooms);
 		System.out.println("Stack: All rooms can be visited - " + canVisitStack);
+
+		boolean canVisitStackAlt = canVisitAllRoomsStackAlt(rooms);
+		System.out.println("Stack Alt: All rooms can be visited - " + canVisitStackAlt);
 	}
 
+	// DFS - Recursion
+	// When room is visited 1st time, look at all keys in that room. If any key
+	// hasn't used, add it to the recursion stack to be used.
+	// Time Complexity - O(n+e), where n is number of rooms, e - keys
+	// Space Complexity - O(n), where n is visited and recursion stack space need
+	// for n rooms.
 	private static boolean canVisitAllRoomsRecursion(List<List<Integer>> rooms) {
 		boolean[] visited = new boolean[rooms.size()];
 		canVisitRecursive(rooms, visited, 0);
@@ -58,21 +67,61 @@ public class P841KeysAndRooms {
 		}
 	}
 
+	private static void canVisit(List<List<Integer>> rooms, boolean[] visited, int key) {
+		visited[key] = true;
+		for (int room : rooms.get(key)) {
+			if (!visited[room]) {
+				canVisit(rooms, visited, room);
+			}
+		}
+	}
+
+	// DFS - Stack
+	// When room is visited 1st time, look at all keys in that room. If any key
+	// hasn't used, add it to the stack to be used.
 	// Time Complexity - O(n+e), where n is number of rooms, e - keys
 	// Space Complexity - O(n), where n is visited and stack space need for n rooms
 	public static boolean canVisitAllRoomsStack(List<List<Integer>> rooms) {
 		Stack<Integer> stack = new Stack<>();
-		for (int num : rooms.get(0)) {
-			stack.push(num);
+		for (int key : rooms.get(0)) {
+			stack.push(key);
 		}
 		boolean[] visited = new boolean[rooms.size()];
 		visited[0] = true;
-		while (!stack.isEmpty()) {
-			int room = stack.pop();
-			if (!visited[room]) {
-				visited[room] = true;
-				for (int num : rooms.get(room)) {
-					stack.push(num);
+		// At start, we've stack(todo list) of keys to use.
+		// Visited represents that we've entered this room.
+		while (!stack.isEmpty()) { // While we've keys
+			int key = stack.pop(); // Get the next key for rooms
+			if (!visited[key]) { // If the key hasn't been used
+				visited[key] = true; // mark that we used the key/entered the room.
+				for (int nextKeys : rooms.get(key)) { // For every key in room #key
+					stack.push(nextKeys); // Add the key to stack
+				}
+			}
+		}
+		// If any room hasn't been visited, return false.
+		for (boolean val : visited) {
+			if (!val) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static boolean canVisitAllRoomsStackAlt(List<List<Integer>> rooms) {
+		int n = rooms.size();
+		boolean[] visited = new boolean[n];
+		visited[0] = true;
+		Stack<Integer> stack = new Stack<>();
+		stack.push(0);
+		// At start, we've stack(todo list) of keys to use.
+		// Visited represents that we've entered this room.
+		while (!stack.isEmpty()) { // While we've keys
+			int key = stack.pop(); // Get the next key for rooms
+			for (int newKey : rooms.get(key)) { // For every key in room #key
+				if (!visited[newKey]) { // If the key hasn't been used
+					visited[newKey] = true; // mark that we used the key/entered the room.
+					stack.push(newKey); // Add the key to stack
 				}
 			}
 		}
@@ -83,5 +132,4 @@ public class P841KeysAndRooms {
 		}
 		return true;
 	}
-
 }
